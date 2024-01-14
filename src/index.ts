456de2042ea2912cbf72c5c1b7ff40f5c280fc90
Matt Kadatz <26133178+mdatz@users.noreply.github.com>
2024-01-13 20:21:05 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import express from 'express';
 
 const app = express();
 app.listen(2077, () => {
-  console.log(`server running on port 2077, safe travels netrunner 🧙`);
+  console.log(`server running on port 2077, safe travels netrunner.`);
 });
