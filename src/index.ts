335f052484a3b43e72dde34af4bc87e6d5b197e8
Matt Kadatz <kadatzmatthew@gmail.com>
2024-12-25 14:37:15 -0700

    Update package, add gg ez api docs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import { apiReference } from '@scalar/express-api-reference'
 
 const port = process.env.PORT || 2077;
 
 const app = express();
+app.use("/", express.static(__dirname + "/public"));
+
+app.use(
+  '/api-docs',
+  apiReference({
+    theme: 'keplar',
+    spec: {
+       url: '/openapi.yaml',
+    },
+  }),
+)
+
 app.listen(port, () => {
   console.log(`server running on port ${port}, safe travels netrunner.`);
 });
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/status', (req: Request, res: Response) => {
   res.send('You don\'t make a name for yourself in night city by how you live...');
 });
